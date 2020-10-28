2c44e2512345589048a1a3cafe03a34d71484271
Sebastien <sebastien.dabdoub@gmail.com>
2020-10-28 16:15:53 -0700

    Initial commit for an l-system engine.
    Adds a library for an L-system that accepts an axiom and a set of rules of
    production. It iterates on the rules and can then generate a set of 3D points
    that emulates the Logo turtle machine. Currently only Forward (F) and Right turn
    (>) are supported.

---
+++ lsystem.py
@@ -0,0 +1,68 @@
+# This is an l-system engine that emulates a 3D Logo turtle.
+# That is, it takes an axiom, rules, and iteration count and returns a set of
+# 3D points that can be connected to form an image.
+# It supports the turtle commands like Forward and turn, but is more performant
+# than running the actual turtle module and works in 3D.
+
+# It is inspired by https://github.com/dkandalov/lsystem-js.
+
+import math
+import numpy
+import quaternion
+from collections import deque
+
+# Represents a 3D point that should not be connected to. This is how we implement
+# brackets in the l-system: http://www1.biologie.uni-hamburg.de/b-online/e28_3/lsys.html.
+DONT_CONNECT = (None, None, None)
+X_AXIS = (1, 0, 0)
+Y_AXIS = (0, 1, 0)
+Z_AXIS = (0, 0, 1)
+
+
+class LSystem:
+    # axiom: str
+    # rules: dict from str -> str
+    # angle: double in degrees
+    # step_length: int
+    def __init__(self, axiom, rules):
+        self.axiom = axiom
+        self.current_grammar = axiom
+        self.rules = rules
+
+    def _IterateGrammar(self, iterations=1):
+        def sub(x):
+            if x in self.rules:
+                return self.rules[x]
+            return x
+        for i in range(iterations):
+            self.current_grammar = "".join([sub(x) for x in self.current_grammar])
+
+    def GeneratePoints(self, angle, step_length, start_point = [0, 0, 0], start_dir = [1, 0, 0]):
+        yield start_point[:]
+
+        point = numpy.array(start_point)
+        step = numpy.array(start_dir) * step_length
+
+        # Note on quaternions to self:
+        # Quaternions are a 4D generalization of complex numbers that can be interpreted
+        # as axis+angle and produce rotations in 3D.
+        # A quaternion vector is (w, x, y, z) where x,y,z is the axis vector and w is related
+        # to the angle: https://stackoverflow.com/a/4023264.
+        # The function from_euler_angles takes euler angles (a, b, g) in radians and constructs
+        # a quaternion according to https://github.com/moble/quaternion/wiki/Euler-angles-are-horrible#conventions.
+        direction = quaternion.from_euler_angles(numpy.array(start_dir) * math.radians(0))
+        stack = deque()
+        
+        for c in self.current_grammar:
+            if c == "F":
+                # Move forward by taking a "step" in the current direction.
+                point = numpy.round(point + quaternion.rotate_vectors(direction, step), 2)
+                yield point.tolist()
+            if c == ">":
+                # Rotate direction by angle along x axis
+                rotation = quaternion.from_euler_angles(numpy.array(X_AXIS) * math.radians(angle))
+                direction = direction * rotation
+        
+
+
+    
---
+++ lsystem_test.py
@@ -0,0 +1,39 @@
+import unittest
+import lsystem
+
+class TestLSystem(unittest.TestCase):
+
+    def test_simple_grammar_iteration(self):
+        ls = lsystem.LSystem("F", {"F": "FF"})
+        
+        self.assertEqual(ls.current_grammar, "F")
+        ls._IterateGrammar()
+        self.assertEqual(ls.current_grammar, "FF")
+        ls._IterateGrammar()
+        self.assertEqual(ls.current_grammar, "FFFF")
+
+        # Case with variable with no rule
+        ls = lsystem.LSystem("F", {"F": "FX"})
+        self.assertEqual(ls.current_grammar, "F")
+        ls._IterateGrammar()
+        self.assertEqual(ls.current_grammar, "FX")
+        ls._IterateGrammar()
+        self.assertEqual(ls.current_grammar, "FXX")
+
+    def test_generate_points(self):
+        ls = lsystem.LSystem("F", {"F": "FF"})
+        ls._IterateGrammar()
+
+        points = [x for x in ls.GeneratePoints(30, 10)]
+        expected_points = [[0, 0, 0], [10,  0,  0], [20,  0,  0]]
+        self.assertEqual(points, expected_points)
+        
+    def test_generate_points_rotation(self):
+        ls = lsystem.LSystem("F>F>F>F", {})
+
+        points = [x for x in ls.GeneratePoints(90, 10)]
+        expected_points = [[0, 0, 0], [10,  0,  0], [10,  10,  0], [0, 10, 0], [0, 0, 0]]
+        self.assertEqual(points, expected_points)
+
+if __name__ == '__main__':
+    unittest.main()
